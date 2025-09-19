531682c6cfebd3e0a9b9ae51f5a5913fb1d0457c
Biwbong <bewrockgame1@gmail.com>
2025-09-19 12:10:57 +0700

    Update auth.ts

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -104,7 +104,7 @@ router.get("/logout", (req: Request, res: Response) => {
 
 // ---------------- VERIFY ----------------
 router.get("/verify", (req: Request, res: Response) => {
-  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+  const token = req.cookies.token;
   if (!token) return res.status(401).json({ error: "ไม่มี token" });
 
   try {
